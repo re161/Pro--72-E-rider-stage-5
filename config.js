2970ee5edfef26deaff03d99e18b5c853654ec85
re161 <77871348+re161@users.noreply.github.com>
2024-06-08 13:53:13 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCGN0qa6gu0xEVyKY4ItwhZMbWr-8frCDc",
+  authDomain: "e-ride-46a4e.firebaseapp.com",
+  projectId: "e-ride-46a4e",
+  storageBucket: "e-ride-46a4e.appspot.com",
+  messagingSenderId: "598592373857",
+  appId: "1:598592373857:web:e60e519b5b475620055620"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
